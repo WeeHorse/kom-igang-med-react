2a415fddc90e59703f4352aa17a12bafc2a32065
WeeHorse <benjaminberglund@gmail.com>
2024-02-16 12:09:27 +0100

    dag2 useEffect list page

--- src/pages/MyUseEffectListPage.jsx
+++ src/pages/MyUseEffectListPage.jsx
@@ -1,13 +1,20 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
 function MyUseEffectListPage(){
 
-  const [items] = useState([
-    {name: "Lamp", brand: "Ikeo", image: "https://cdn.starwebserver.se/shops/sekelskifte/files/led-lampa-112-977.jpg"},
-    {name: "Table", brand: "Ikeo", image: "https://assets.hermes.com/is/image/hermesproduct/les-necessaires-d-hermes-satellite-table-small-model--960216M%2001-worn-2-0-0-1000-1000_g.jpg"},
-    {name: "Chair", brand: "Ilvo", image: "https://www.ikea.com/se/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s"},
-  ])
+  // körs vid varje (om)rendering, körs när man lämnar komponenten
+  useEffect(()=>{
+    async function load(){
+      const response = await fetch('data.json')
+      const items = await response.json()
+      console.log(items)
+      setItems(items)
+    }
+    load()
+  }, []) // tom array som andra argument = körs endast vid komponentladdning
+
+  const [items, setItems] = useState([])
 
   return <div id="my-list-page">
     <h1>MyUseEffectListPage</h1>
